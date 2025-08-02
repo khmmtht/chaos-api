dc2a543ceccdaa88fab1e27a75d978fe24094dc2
khemmathat.ph <khemmathat141@gmail.com>
2025-08-02 22:22:00 +0700

    chore: add condition between 2 drivers

--- routes/api/chaos.go
+++ routes/api/chaos.go
@@ -2,15 +2,24 @@ package api
 
 import (
 	"chaos-api/adapter"
+	_interface "chaos-api/adapter/interface"
 	_const "chaos-api/const"
 	"chaos-api/handler"
 	"chaos-api/middleware"
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"os"
 )
 
 func AddChaosRoutes(e *echo.Group, client *mongo.Client) {
-	chaosHandler := handler.NewChaosHandler(adapter.NewMongoDbChaosConfigAdapter(client))
+	var configAdapter _interface.ChaosConfigAdapter
+	if os.Getenv("DRIVER") == "mongodb" {
+		configAdapter = adapter.NewMongoDbChaosConfigAdapter(client)
+	} else {
+		configAdapter = adapter.NewMemoryChaosConfigAdapter()
+	}
+
+	chaosHandler := handler.NewChaosHandler(configAdapter)
 	tokenMiddleware := middleware.NewProjectTokenMiddleware(adapter.NewMongoDbTokenAdapter(client))
 
 	g := e.Group("/"+_const.ApiVersion+"/chaos", tokenMiddleware.Handler())
--- routes/api/project.go
+++ routes/api/project.go
@@ -2,14 +2,26 @@ package api
 
 import (
 	"chaos-api/adapter"
+	_interface "chaos-api/adapter/interface"
 	_const "chaos-api/const"
 	"chaos-api/handler"
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"os"
 )
 
 func AddProjectRoutes(e *echo.Group, client *mongo.Client) {
-	projectHandler := handler.NewProject(adapter.NewMongoDbProjectAdapter(client), adapter.NewMongoDbTokenAdapter(client))
+	var projectAdapter _interface.ProjectAdapter
+	var tokenAdapter _interface.TokenAdapter
+	if os.Getenv("DRIVER") == "mongodb" {
+		projectAdapter = adapter.NewMongoDbProjectAdapter(client)
+		tokenAdapter = adapter.NewMongoDbTokenAdapter(client)
+	} else {
+		projectAdapter = adapter.NewMemoryProjectAdapter()
+		tokenAdapter = adapter.NewMemoryTokenAdapter()
+	}
+
+	projectHandler := handler.NewProject(projectAdapter, tokenAdapter)
 
 	g := e.Group("/" + _const.ApiVersion + "/admin/project")
 
